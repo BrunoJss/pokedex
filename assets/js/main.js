e276fc7c5d96629fb1a2c3c2a77d9b5eced0707c
brunojss <bruno.jsscruz@gmail.com>
2025-08-25 23:09:57 -0300

    Requisitando informações da poke api, utilizando map e join para transformar listas

---
+++ assets/js/main.js
@@ -0,0 +1,32 @@
+
+function convertPokemonTypeToLi(pokemonTypes) {
+    return pokemonTypes.map((typeSlot) => `<li class="type">${typeSlot.type.name}</li>`)
+}
+
+function convertPokemonToLi(pokemon) {
+    return `
+            <li class="pokemon">
+                <span class="number">#${pokemon.order}</span>
+                <span class="name">${pokemon.name}</span>
+                <div class="detail">
+                    <ol class="types">
+                    ${convertPokemonTypeToLi(pokemon.types).join('')}
+                    </ol>
+                    <img src="${pokemon.sprites.other.dream_world.front_default}"
+                    alt="${pokemon.name}">
+                </div>
+            </li>
+            `
+}
+
+
+pokeApi.getPokemons().then((pokemonList = []) => {
+
+    const pokemonOl = document.getElementById('pokemonList')
+    pokemonOl.innerHTML += pokemonList.map(convertPokemonToLi).join("")
+
+
+})
+    .catch((error) => console.log(error))
+    .finally(() => console.log("Requisição finalizada.")
+    )
---
+++ assets/js/pokeapi.js
@@ -0,0 +1,18 @@
+const pokeApi = {}
+
+pokeApi.getPokemonsDetails = (pokemon) => {
+    return fetch(pokemon.url)
+        .then((response) => response.json())
+}
+
+
+pokeApi.getPokemons = (offset = 0, limit = 5) => {
+    const url = `https://pokeapi.co/api/v2/pokemon?offset=${offset}&limit=${limit}`
+    return fetch(url)
+        .then((response) => response.json())
+        .then((responseBody) => responseBody.results)
+        .then((pokemons) => pokemons.map(pokeApi.getPokemonsDetails))
+        .then((detailRequests) => Promise.all(detailRequests))
+        .then((pokemonsDetails) =>pokemonsDetails)
+        .catch((error) => console.log(error))
+}
